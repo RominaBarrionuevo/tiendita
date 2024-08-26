5b13d43f478b4dfe9e80f5bce96c7920eef70c2a
Romina Alejandra Barrionuevo <robarrionuevo13@gmail.com>
2024-08-26 18:06:01 -0300

    css

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import "./ItemListContainer.css"
 
 const ItemListContainer = (props) => {
   return (
-    <div>
-        <h2>{props.greeting}</h2>
+    <div>   
+     <h2 className='propsColor'>{props.greeting}</h2>
     </div>
   )
-}
+} 
+
+/*  <h2 style={{color:"orange"}}>{props.greeting}</h2> */
+
+/* 
+opcion dos de desestructuracion
+
+const ItemListContainer = ({greeting}) => {
+  return (
+    <div>   
+     <h2 className='propsColor'>{greeting}</h2>
+    </div>
+  )
+} */ 
 
 export default ItemListContainer
